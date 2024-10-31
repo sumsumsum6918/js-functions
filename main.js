795047360be49ecf92d6a9cc3ae0089f8277a204
sumsumsum6918 <tinafongfong@gmail.com>
2024-10-31 11:38:13 +0100

    1-12

---
+++ main.js
@@ -0,0 +1,86 @@
+const generateRandomNumber = (array) => {
+  return (randomNumber = Math.floor(Math.random() * array.length));
+};
+
+//Level Easy"
+//Segment 1: Functions with No Parameters and No Return Values
+
+//1. Basic Greeting");
+const greet = () => {
+  return "Hello, World!";
+};
+
+//2. Favorite Number");
+const favoriteNumber = () => {
+  return 6;
+};
+
+//3. Magic Eight Ball");
+const magicEightBallResponses = ["Yes", "No", "Maybe", "Ask again later"];
+const magicEightBall = () => {
+  const randomNumber = generateRandomNumber(magicEightBallResponses);
+  return magicEightBallResponses[randomNumber];
+};
+
+//"4. Current Year");
+const getCurrentYear = () => {
+  return new Date().getFullYear();
+};
+
+//5. Random Joke");
+const jokes = [
+  "joke1",
+  "joke2",
+  "joke3",
+  "joke4",
+  "joke5",
+  "joke6",
+  "joke7",
+  "joke8",
+  "joke9",
+  "joke10",
+];
+const tellJoke = () => {
+  const randomNumber = generateRandomNumber(jokes);
+  return jokes[randomNumber];
+};
+
+//Segment 2: Functions with Parameters (One or More)
+//6. Personalized Greeting");
+const personalGreeting = (username) => {
+  return `Hello, ${username}!`;
+};
+
+//7. Sum of Two Numbers");
+const add = (number1, number2) => {
+  return number1 + number2;
+};
+
+//8. Age in Months");
+const ageInMonths = (age) => {
+  return age * 12 + "months";
+};
+
+//9. Temperature Converter");
+const celsiusToFahrenheit = (celsiusTemp) => {
+  return (9 / 5) * celsiusTemp + 32 + "F";
+};
+
+//10. Full Name");
+const getFullName = (firstName, lastName) => {
+  return firstName + " " + lastName;
+};
+
+//Segment 3: Functions with Parameters and Return Values
+//11. Area of a Rectangle");
+const calculateArea = (length, width) => {
+  return length * width;
+};
+
+//12. Find Maximum");
+const findMax = (number1, number2) => {
+  return number1 > number2 ? number1 : number2;
+};
+
+//13. Count Vowels
+const countVowels = () => {};
